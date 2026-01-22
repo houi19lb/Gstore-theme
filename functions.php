e27bfcec1870b4b71d3cb8734fae5b185372a93c
Matheus Peixoto <matheuspmenezes2000@gmail.com>
2026-01-22 16:09:10 -0300

    ajustando versao de celular carrinho15

--- functions.php
+++ functions.php
@@ -4542,6 +4542,20 @@ function gstore_calculate_shipping_ajax() {
 	$debug_log_path = function_exists( 'get_theme_file_path' ) ? get_theme_file_path( '.cursor/debug.log' ) : '';
 	$debug_enabled = ! empty( $_REQUEST['debug'] ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 	// #region agent log
+	if ( function_exists( 'wp_json_encode' ) ) {
+		@file_put_contents( 'C:\\Users\\mathe\\Gstore-theme\\.cursor\\debug.log', wp_json_encode( array(
+			'location'     => 'functions.php:gstore_calculate_shipping_ajax',
+			'message'      => 'ajax handler invoked (absolute path)',
+			'data'         => array(
+				'requestKeys' => array_keys( $_REQUEST ),
+				'debugParam'  => isset( $_REQUEST['debug'] ) ? $_REQUEST['debug'] : null,
+			),
+			'timestamp'    => (int) round( microtime( true ) * 1000 ),
+			'sessionId'    => 'debug-session',
+			'runId'        => 'run1',
+			'hypothesisId' => 'H2',
+		) ) . PHP_EOL, FILE_APPEND ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_read_file_system_write_file
+	}
 	if ( $debug_log_path && function_exists( 'wp_json_encode' ) ) {
 		$log_payload = array(
 			'location'     => 'functions.php:gstore_calculate_shipping_ajax',
