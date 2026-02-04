3abfbd34ab263b7d3f461de790189c664638e151
Matheus Peixoto <matheuspmenezes2000@gmail.com>
2026-02-04 00:50:02 -0300

    erro wp

--- functions.php
+++ functions.php
@@ -3206,7 +3206,7 @@ function gstore_enqueue_checkout_assets() {
 		if ( function_exists( 'is_cart' ) && is_cart() ) {
 			$product_id = 0;
 			$quantity   = 1;
-		} elseif ( is_product() && $product ) {
+		} elseif ( is_product() && $product && is_object( $product ) && method_exists( $product, 'get_id' ) ) {
 			$product_id = $product->get_id();
 		}
 
