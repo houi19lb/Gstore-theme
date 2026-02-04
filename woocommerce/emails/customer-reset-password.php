f6d7b7289ef7668b56227c4d19010bf39530e943
Matheus Peixoto <matheuspmenezes2000@gmail.com>
2026-02-04 18:04:46 -0300

    envio de email 4

--- woocommerce/emails/customer-reset-password.php
+++ woocommerce/emails/customer-reset-password.php
@@ -1,30 +1,69 @@
 <?php
 /**
- * E-mail de redefinição de senha (cliente).
+ * Customer Reset Password email
  *
- * Este template pode ser editado em seu tema em:
- * yourtheme/woocommerce/emails/customer-reset-password.php
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-reset-password.php.
  *
- * Variáveis disponíveis: $user_login, $reset_url, $blogname, $email_heading, $email, $additional_content.
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs, the version of the template file will be bumped and
+ * the readme will list any important changes.
  *
- * @package GStore
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 10.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
 
-do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
-
-<?php /* translators: %s: Customer username */ ?>
-<p><?php printf( esc_html__( 'Olá %s,', 'gstore' ), esc_html( $user_login ) ); ?></p>
-<p><?php printf( esc_html__( 'Foi solicitada a redefinição da senha da sua conta em %s.', 'gstore' ), esc_html( $blogname ) ); ?></p>
-<p><?php esc_html_e( 'Clique no link abaixo para definir uma nova senha:', 'gstore' ); ?></p>
-<p><a href="<?php echo esc_url( $reset_url ); ?>"><?php esc_html_e( 'Redefinir senha', 'gstore' ); ?></a></p>
-<p><?php esc_html_e( 'Se você não solicitou isso, ignore este e-mail. O link expira em breve.', 'gstore' ); ?></p>
-
-<?php
-if ( ! empty( $additional_content ) ) {
-	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
+
+$email_improvements_enabled = FeaturesUtil::feature_is_enabled( 'email_improvements' );
+
 ?>
 
-<?php do_action( 'woocommerce_email_footer', $email ); ?>
+<?php do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+
+<?php echo $email_improvements_enabled ? '<div class="email-introduction">' : ''; ?>
+<?php /* translators: %s: Customer username */ ?>
+<p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $user_login ) ); ?></p>
+<?php /* translators: %s: Store name */ ?>
+<p><?php printf( esc_html__( 'Someone has requested a new password for the following account on %s:', 'woocommerce' ), esc_html( $blogname ) ); ?></p>
+<?php if ( $email_improvements_enabled ) : ?>
+	<div class="hr hr-top"></div>
+	<?php /* translators: %s: Username */ ?>
+	<p><?php echo wp_kses( sprintf( __( 'Username: <b>%s</b>', 'woocommerce' ), esc_html( $user_login ) ), array( 'b' => array() ) ); ?></p>
+	<div class="hr hr-bottom"></div>
+	<p><?php esc_html_e( 'If you didn\'t make this request, just ignore this email. If you\'d like to proceed, reset your password via the link below:', 'woocommerce' ); ?></p>
+<?php else : ?>
+	<?php /* translators: %s: Customer username */ ?>
+	<p><?php printf( esc_html__( 'Username: %s', 'woocommerce' ), esc_html( $user_login ) ); ?></p>
+	<p><?php esc_html_e( 'If you didn\'t make this request, just ignore this email. If you\'d like to proceed:', 'woocommerce' ); ?></p>
+<?php endif; ?>
+<p>
+	<a class="link" href="<?php echo esc_url( add_query_arg( array( 'key' => $reset_key, 'id' => $user_id, 'login' => rawurlencode( $user_login ) ), wc_get_endpoint_url( 'lost-password', '', wc_get_page_permalink( 'myaccount' ) ) ) ); ?>"><?php // phpcs:ignore WordPress.Arrays.ArrayDeclarationSpacing.AssociativeArrayFound ?>
+		<?php
+		if ( $email_improvements_enabled ) {
+			esc_html_e( 'Reset your password', 'woocommerce' );
+		} else {
+			esc_html_e( 'Click here to reset your password', 'woocommerce' );
+		}
+		?>
+	</a>
+</p>
+<?php echo $email_improvements_enabled ? '</div>' : ''; ?>
+
+<?php
+/**
+ * Show user-defined additional content - this is set in each email's settings.
+ */
+if ( $additional_content ) {
+	echo $email_improvements_enabled ? '<table border="0" cellpadding="0" cellspacing="0" width="100%" role="presentation"><tr><td class="email-additional-content email-additional-content-aligned">' : '';
+	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+	echo $email_improvements_enabled ? '</td></tr></table>' : '';
+}
+
+do_action( 'woocommerce_email_footer', $email );
