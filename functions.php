220f3f8575a09976d6483c56bdcc689166d4465d
Matheus Peixoto <matheuspmenezes2000@gmail.com>
2026-02-05 15:05:22 -0300

    titulo Cards3

--- functions.php
+++ functions.php
@@ -3206,7 +3206,7 @@ function gstore_enqueue_checkout_assets() {
 		if ( function_exists( 'is_cart' ) && is_cart() ) {
 			$product_id = 0;
 			$quantity   = 1;
-		} elseif ( is_product() && $product && is_a( $product, 'WC_Product' ) ) {
+		} elseif ( is_product() && $product ) {
 			$product_id = $product->get_id();
 		}
 
