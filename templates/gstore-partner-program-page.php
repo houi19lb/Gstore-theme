1ed4923ec0d07aca8bfdc528d61438fd1f3ac88d
Matheus Peixoto <matheuspmenezes2000@gmail.com>
2026-06-01 14:23:32 -0300

    ajuste revendedor4

--- templates/gstore-partner-program-page.php
+++ templates/gstore-partner-program-page.php
@@ -6,11 +6,27 @@
  */
 
 defined( 'ABSPATH' ) || exit;
-
-get_header();
-
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class( 'gstore-partner-program-body' ); ?>>
+<?php
+if ( function_exists( 'wp_body_open' ) ) {
+	wp_body_open();
+}
+?>
+<?php
 if ( function_exists( 'gstore_partner_account_render_application_page' ) ) {
 	gstore_partner_account_render_application_page();
 }
-
-get_footer();
+?>
+<?php
+echo do_blocks( '<!-- wp:template-part {"slug":"footer","area":"footer"} /-->' );
+?>
+<?php wp_footer(); ?>
+</body>
+</html>
